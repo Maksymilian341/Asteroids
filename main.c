93c5254d3715acaac6bdbcb6869395f2884d389f
Maksymilian341 <maksymiliangorny2006@gmail.com>
2026-04-30 17:11:42 +0200

    asteroids

--- main.c
+++ main.c
@@ -1,88 +1,90 @@
 #include "raylib.h"
 #include <math.h>
 
-#define szerokosc 800
-#define wysokosc 600
-#define maxPocisk 10
+#define screenWidth 800
+#define screenHeight 600
+#define maxBullets 10
 
-typedef struct Zawodnik {
-    Vector2 pozycja;
-    Vector2 predkosc;
-    float rotacja;
-    float srednica;
-} Zawdonik;
+typedef struct Player{
+    Vector2 position;
+    Vector2 velocity;
+    float rotation;
+    float radius;
+}Player;
 
-typedef struct Pocisk {
-    Vector2 pozycja;
-    Vector2 predkosc;
-    bool aktywny;
-} Pocisk;
+typedef struct Bullet{
+    Vector2 position;
+    Vector2 velocity;
+    bool active;
+}Bullet;
 
 
 int main(void) {
-    InitWindow(szerokosc, wysokosc, "Asteroids - Maks");
+    InitWindow(screenWidth, screenHeight, "Asteroids - Maks");
 
-    Zawdonik zawdonik = {0};
-    zawdonik.pozycja = (Vector2){szerokosc/2, wysokosc/2};
-    zawdonik.predkosc = (Vector2){0, 0};
-    zawdonik.rotacja = 0.0f; 
-    zawdonik.srednica = 15.0f;
+    Player player = {0};
+    player.position = (Vector2){screenWidth/2, screenHeight/2};
+    player.velocity = (Vector2){0, 0};
+    player.rotation = 0.0f; 
+    player.radius = 15.0f;
 
-    Pocisk pocisk[maxPocisk] = {0};
+    Bullet bullets[MAX_BULLETS] = {0};
 
     SetTargetFPS(60); 
 
     while (!WindowShouldClose()){
 
-            if (IsKeyDown(KEY_LEFT)) zawdonik.rotacja -= 4.5f;
-
-            if (IsKeyDown(KEY_RIGHT)) zawdonik.rotacja += 4.5f; 
-
+            if (IsKeyDown(KEY_LEFT)){
+                player.rotation -= 4.5f;
+            }
+            if (IsKeyDown(KEY_RIGHT)){
+                player.rotation += 4.5f; 
+            }
             if (IsKeyDown(KEY_UP)){
-            zawdonik.predkosc.x += cosf((zawdonik.rotacja - 90) * DEG2RAD) * 0.1f;
-            zawdonik.predkosc.y += sinf((zawdonik.rotacja - 90) * DEG2RAD) * 0.1f;
-        }
-
-            zawdonik.pozycja.x += zawdonik.predkosc.x;
-            zawdonik.pozycja.y += zawdonik.predkosc.y;
-
-            if (zawdonik.pozycja.x > szerokosc){
-                zawdonik.pozycja.x = 0;
-            } 
-            else if (zawdonik.pozycja.x < 0){
-                zawdonik.pozycja.x = szerokosc;
+            player.velocity.x += cosf((player.rotation - 90) * DEG2RAD) * 0.1f;
+            player.velocity.y += sinf((player.rotation - 90) * DEG2RAD) * 0.1f;
             }
 
-            if (zawdonik.pozycja.y > wysokosc){
-                zawdonik.pozycja.y = 0;
+            player.position.x += player.velocity.x;
+            player.position.y += player.velocity.y;
+
+            if (player.position.x > screenWidth){
+                player.position.x = 0;
             } 
-            else if (zawdonik.pozycja.y < 0){
-                zawdonik.pozycja.y = wysokosc;
+            else if (player.position.x < 0){
+                player.position.x = screenWidth;
+            }
+
+            if (player.position.y > screenHeight){
+                player.position.y = 0;
+            } 
+            else if (player.position.y < 0){
+                player.position.y = screenHeight;
             }
 
             if (IsKeyPressed(KEY_SPACE)){
-                for (int i = 0;i < maxPocisk;i++){
-                    if (!pocisk[i].aktywny){
-                        pocisk[i].aktywny = true;
+                for (int i = 0;i < maxBullets;i++){
+                    if (!bullets[i].active){
+                        bullets[i].active = true;
 
-                        pocisk[i].pozycja.x = zawdonik.pozycja.x + cosf((zawdonik.rotacja - 90) * DEG2RAD) * zawdonik.srednica;
-                        pocisk[i].pozycja.y = zawdonik.pozycja.y + sinf((zawdonik.rotacja - 90) * DEG2RAD) * zawdonik.srednica;
+                        bullets[i].position.x = player.position.x + cosf((player.rotation - 90) * DEG2RAD) * player.radius;
+                        bullets[i].position.y = player.position.y + sinf((player.rotation - 90) * DEG2RAD) * player.radius;
                         
-                        pocisk[i].predkosc.x = cosf((zawdonik.rotacja - 90) * DEG2RAD) * 7.0f;
-                        pocisk[i].predkosc.y = sinf((zawdonik.rotacja - 90) * DEG2RAD) * 7.0f;
+                        bullets[i].velocity.x = cosf((player.rotation - 90) * DEG2RAD) * 7.0f;
+                        bullets[i].velocity.y = sinf((player.rotation - 90) * DEG2RAD) * 7.0f;
                         break; 
                     }
                 }
             }
 
-            for (int i = 0; i < maxPocisk; i++){
-                if (pocisk[i].aktywny){
-                    pocisk[i].pozycja.x += pocisk[i].predkosc.x;
-                    pocisk[i].pozycja.y += pocisk[i].predkosc.y;
+            for (int i = 0; i < maxBullets; i++){
+                if (bullets[i].active){
+                    bullets[i].position.x += bullets[i].velocity.x;
+                    bullets[i].position.y += bullets[i].velocity.y;
 
-                if (pocisk[i].pozycja.x < 0 || pocisk[i].pozycja.x > szerokosc ||
-                    pocisk[i].pozycja.y < 0 || pocisk[i].pozycja.y > wysokosc){
-                    pocisk[i].aktywny = false;
+                if (bullets[i].position.x < 0 || bullets[i].position.x > screenWidth ||
+                    bullets[i].position.y < 0 || bullets[i].position.y > screenHeight){
+                    bullets[i].active = false;
                 }
                 }
             }
@@ -91,25 +93,25 @@ int main(void) {
             ClearBackground(BLACK);
 
             Vector2 v1 = { 
-            zawdonik.pozycja.x + cosf((zawdonik.rotacja - 90) * DEG2RAD) * zawdonik.srednica, 
-            zawdonik.pozycja.y + sinf((zawdonik.rotacja - 90) * DEG2RAD) * zawdonik.srednica 
+            player.position.x + cosf((player.rotation - 90) * DEG2RAD) * player.radius, 
+            player.position.y + sinf((player.rotation - 90) * DEG2RAD) * player.radius 
             };
 
             Vector2 v2 = { 
-                zawdonik.pozycja.x + cosf((zawdonik.rotacja + 45) * DEG2RAD) * zawdonik.srednica, 
-                zawdonik.pozycja.y + sinf((zawdonik.rotacja + 45) * DEG2RAD) * zawdonik.srednica 
+                player.position.x + cosf((player.rotation + 30) * DEG2RAD) * player.radius, 
+                player.position.y + sinf((player.rotation + 30) * DEG2RAD) * player.radius 
             };
 
             Vector2 v3 = { 
-                zawdonik.pozycja.x + cosf((zawdonik.rotacja + 135) * DEG2RAD) * zawdonik.srednica, 
-                zawdonik.pozycja.y + sinf((zawdonik.rotacja + 135) * DEG2RAD) * zawdonik.srednica 
+                player.position.x + cosf((player.rotation + 150) * DEG2RAD) * player.radius, 
+                player.position.y + sinf((player.rotation + 150) * DEG2RAD) * player.radius 
             };
 
             DrawTriangleLines(v1, v2, v3, RAYWHITE);
             
-            for (int i = 0; i < maxPocisk;i++){
-                if (pocisk[i].aktywny){
-                    DrawCircleV(pocisk[i].pozycja, 2, RED);
+            for (int i = 0; i < maxBullets;i++){
+                if (bullets[i].active){
+                    DrawCircleV(bullets[i].position, 2, RED);
                 }   
             }
 
